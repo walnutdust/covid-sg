597ba7dc23b977aee2e56d8ec6641882c548bde7
Garett Tok <garettok@hotmail.com>
2020-04-11 15:21:13 +0800

    Updates

---
+++ precache-manifest.bc9104224ae67160de5636ae1c647fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd409eacd8afb3b96a3580eef7de5b4",
+    "url": "/covid-sg/index.html"
+  },
+  {
+    "revision": "4c7400f7225f4bfcc538",
+    "url": "/covid-sg/static/css/main.ab78a75e.chunk.css"
+  },
+  {
+    "revision": "85e84e77cf3f6112eaf0",
+    "url": "/covid-sg/static/js/2.b65c08ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-sg/static/js/2.b65c08ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7400f7225f4bfcc538",
+    "url": "/covid-sg/static/js/main.11f63faa.chunk.js"
+  },
+  {
+    "revision": "d174b978412fcde3dbfa",
+    "url": "/covid-sg/static/js/runtime-main.9f9cb074.js"
+  }
+]);
